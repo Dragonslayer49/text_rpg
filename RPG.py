e3728c4f32715aead14d59c36b0a64a5abb4ea3e
natan <natan.smolka@vp.pl>
2024-01-13 15:34:15 +0100

    Mniej pokazanego tekstu nie ma ciaglego you find yopurself at the entrance

--- RPG.py
+++ RPG.py
@@ -36,66 +36,45 @@ class StartingArea:
         self.current_location = "Entrance"
         self.locations = {
             "Entrance": [['You find yourself at the entrance of the forest, there are two paths before you'],
-                         ["North Path", "South Path"]],
+                         ["North Path", "South Path"], ["grass is very nice"]],
             "North Path": [['while walking through the path you see a abandoned cave entrance'],
-                           ["Entrance", "Cave Entrance"]],
+                           ["Entrance", "Cave Entrance"], ["cave seems to emanate with bad energy"]],
             "South Path": [['you spot a river while walking through the path'], ["Entrance", "River"]],
             "Cave Entrance": [['You enter the dark cave, a goblin blocks your way'],
-                              ["North Path", "Fight Goblin", "South Path"]],
-            "Fight Goblin": [['You are in a battle'], ["Fight", "Flee"]],
-            "River": [['You reach the river, a bridge is visible in the distance'], ["North Path", "Cross Bridge"]],
+                              ["North Path", "Fight Goblin"],
+                              ['goblin looks at you but doesnt seem to attack at least unprovoked']],
+            "River": [['You reach the river, a bridge is visible in the distance'], ["South Path", "Cross Bridge"]],
             "Cross Bridge": [['You cross the bridge and find a peaceful meadow'], ["River", "Meadow"]],
-            "Meadow": [['You are in a beautiful meadow'], ["North Path", "South Path"]],
-            "Fight": [['You are in a battle'], ["Entrance", "River"]],
-            "Flee": [['You wish you would escape a fight'], ["North Path", "SouthPath"]],
+            "Meadow": [['You are in a beautiful meadow'], ["Entance"], ["meadow is nice"]],
         }
         self.tutorial_location = "house"
         self.tutorial_locations = {
             "House": ["Forest"]
         }
 
-    def tutorial(self):
-        choice = input(f"{Fore.CYAN}would you like to do a tutorial? Y/N")
-        if choice == "Y":
-            enemies = enemyLib("monsters.txt")
-            enemy = random.choice(enemies)
-            print(f"{Fore.GREEN}You find yourself at a village, You see a smoke in the distance")
-            print(f"{Fore.CYAN}1-check out the smoke")
-            print(f"{Fore.LIGHTGREEN_EX}2-rest at your house")
-            print(f"{Fore.CYAN}3-go to the forest")
-            choice = input()
-            match choice:
-                case "1":
-                    print(f"{Fore.GREEN}you see soldiers attacking a nearby village")
-                    print(f"{Fore.GREEN}while you looked at the soldiers one noticed you and started going your way")
-                    print(f"{Fore.RED}1-prepare for battle")
-                    print(f"{Fore.YELLOW}2-run away")
-                    choice = input()
-                    if choice == "1":
-                        self.fight(enemy)
-                case "2":
-                    print(f"{Fore.GREEN}while resting a soldier went into you house with a sword in his hand")
-                case "3":
-                    print()
-
-        else:
-            self.navigate()
-
     def navigate(self):
+        numerek = 0
         while player.isAlive():
             available_paths = self.locations[self.current_location][1]
-            print("\n".join(self.locations[self.current_location][0]))
-            print("Available paths:", ", ".join(self.locations[self.current_location][1]))
-            print(f"{Fore.BLUE}write Inventory to Check Inventory")
-            print(f"{Fore.YELLOW}q to Quit\n")
+            if numerek == 0:
+                print("\n".join(self.locations[self.current_location][0]))
+                print("Available paths:", ", ".join(self.locations[self.current_location][1]))
+                numerek += 1
             choice = input()
             if choice in available_paths:
                 self.current_location = choice
                 self.handle_event()
             elif choice == "q":
                 break
+            elif choice == "l":
+                print(self.locations[self.current_location][2])
             elif choice == "Inventory" or choice == "i" or choice == "inventory":
                 player.show_weapon()
+            elif choice == "help":
+                print(f"{Fore.GREEN}To go into any place write the name of the place")
+                print(f"{Fore.BLUE}write Inventory to Check Inventory")
+                print(f"{Fore.YELLOW}to look around or investigate write look or l")
+                print(f"{Fore.RED}q to Quit\n")
             else:
                 print("Invalid")
 
@@ -105,10 +84,11 @@ class StartingArea:
 
     def handle_event(self):
         event = random.randint(1, 10)
-        if event > 1:
+        if event > 9:
             self.encounter()
         else:
             print(f"{Fore.BLUE}you walked without problems.")
+            self.navigate()
 
     def encounter(self):
 
@@ -122,6 +102,7 @@ class StartingArea:
             success = random.randint(1, 10)
             if success > 1:  # potem zmienic zeby byla szansa na atak przeciwnika
                 print(f"{Fore.BLUE}you successfully evaded ", enemy.species)
+                self.navigate()
         elif choice == "1":
             self.fight(enemy)
 
@@ -147,6 +128,59 @@ class StartingArea:
         if not player.isAlive():
             print(f"{Fore.RED}Game Over! You have been defeated.")
             pygame.mixer.Sound("death.wav").play()
+        else:
+            self.navigate()
+
+    def cave(self):
+        print("you are walking through the cave until you see a big monster")
+        print("the monster seems to be asleep near him you see a chest with gold")
+        print("1-try to sneak past and get the gold\n2-sneak atack the big monster\n3-escape while he still sleeps")
+        choice = input()
+        match choice:
+            case '1':
+                print(
+                    "big monster seems to be in a very deep sleep and doesnt notice you taking the "
+                    "chest\nunfortunately you didnt notice that the chest wast tied to the wall and you trip")
+                print("big monster wakes up")
+                self.bossfight()
+            case '3':
+                print("the monster seems to be in a very deep sleep and doesnt wake up while you escape")
+                self.navigate()
+
+    def bossfight(self):
+        print("bossfight here")
+        print('after defeating the boss you return to the cave entrance')
+        self.locations['cave entrance']= [['the cave you have been in seems to be blocked by rocks'],
+                                          ["North Path", "South Path"]]
+        self.navigate()
+
+    def river(self):
+        print("river")
+
+    def tutorial(self):
+        choice = input(f"{Fore.CYAN}would you like to do a tutorial? Y/N")
+        if choice == "Y":
+            enemies = enemyLib("monsters.txt")
+            enemy = random.choice(enemies)
+            print(f"{Fore.GREEN}You find yourself at a village, You see a smoke in the distance")
+            print(f"{Fore.CYAN}1-check out the smoke")
+            print(f"{Fore.LIGHTGREEN_EX}2-rest at your house")
+            print(f"{Fore.CYAN}3-go to the forest")
+            choice = input()
+            match choice:
+                case "1":
+                    print(f"{Fore.GREEN}you see soldiers attacking a nearby village")
+                    print(f"{Fore.GREEN}while you looked at the soldiers one noticed you and started going your way")
+                    print(f"{Fore.RED}1-prepare for battle")
+                    print(f"{Fore.YELLOW}2-run away")
+                    choice = input()
+                    if choice == "1":
+                        self.fight(enemy)
+                case "2":
+                    print(f"{Fore.GREEN}while resting a soldier went into you house with a sword in his hand")
+                case "3":
+                    print()
+
         else:
             self.navigate()
 
